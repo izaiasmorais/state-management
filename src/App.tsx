a648525b62dcdd0c724793ed8887332a2a19b3cf
Izaías Lima <izaiaslima356@gmail.com>
2023-02-07 16:41:07 -0300

    build: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./styles/global.css";
+
+function App() {
+	return (
+		<div className="w-full h-[100vh] bg-[#f5f5f5] ">
+			<div className="w-full max-w-[500px] mx-auto">
+				<h1 className="text-2xl mb-2">Hello World</h1>
+				<button className="bg-purple-500 px-4 py-2 text-white rounded">
+					Hello!
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
